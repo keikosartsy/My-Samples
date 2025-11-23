23b8a3239594b0e20905453460fdef4c4bb002f5
keikosartsy <keikosartsy@gmail.com>
2025-11-22 22:02:53 -1000

    Add files via upload

---
+++ proj01/cmake-build-debug/CMakeFiles/proj01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj01.dir/main.cpp.obj"
+  "libproj01.dll.a"
+  "proj01.exe"
+  "proj01.exe.manifest"
+  "proj01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proj01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
